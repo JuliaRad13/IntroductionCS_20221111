c777e9a055d7979be780eb3f2246111145b6d0c4
радаева юлия <lulka-malish@mail.ru>
2022-11-15 12:25:06 +0300

    003

---
+++ 003/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите число a:");
+double a=Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите число b:");
+double b=Convert.ToDouble(Console.ReadLine());
+
+double max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+
+System.Console.Write("max = ");
+System.Console.WriteLine(max);
