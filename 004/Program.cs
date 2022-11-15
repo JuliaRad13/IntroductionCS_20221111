2f7d174197c2480766c86030615f64cea71693ee
радаева юлия <lulka-malish@mail.ru>
2022-11-15 15:48:27 +0300

    004

---
+++ 004/Program.cs
@@ -0,0 +1,32 @@
+﻿System.Console.WriteLine("Введите число:");
+string week = Console.ReadLine();
+
+if (week == "1")
+{
+System.Console.WriteLine("Понедельник");
+}
+
+if (week == "2")
+{
+System.Console.WriteLine("Вторник");
+}
+if (week == "3")
+{
+System.Console.WriteLine("Среда");
+}
+if (week == "4")
+{
+System.Console.WriteLine("Четверг");
+}
+if (week == "5")
+{
+System.Console.WriteLine("Пятница");
+}
+if (week == "6")
+{
+System.Console.WriteLine("Суббота");
+}
+if (week == "7")
+{
+System.Console.WriteLine("Воскресенье");
+}
