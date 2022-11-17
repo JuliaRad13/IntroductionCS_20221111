6e96e06ca14d73c2b2d98d50813b64bf3a72901b
радаева юлия <lulka-malish@mail.ru>
2022-11-17 16:47:13 +0300

    012.2

--- 012/Program.cs
+++ 012/Program.cs
@@ -1,7 +1,8 @@
 ﻿//12.С клавиатуры вводится целое число из  диапазона  [10, 99]. 
 //Показать наибольшую цифру числа
-
+int n;
 do
 {
-    int n=Convert.ToInt32(());
-}
+    System.Console.WriteLine("Введите число:");
+    n=Convert.ToInt32(Console.ReadLine());
+} while (n<10 || n>99);
