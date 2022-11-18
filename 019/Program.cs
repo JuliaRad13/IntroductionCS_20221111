7324246de4f4e8fd9dfc2e89adf8e18ec0a4b4ab
радаева юлия <lulka-malish@mail.ru>
2022-11-18 16:13:49 +0300

    019+

---
+++ 019/Program.cs
@@ -0,0 +1,4 @@
+﻿// 19. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y 
+//!(X || Y)==!X && !Y
+System.Console.WriteLine("X Y ¬(X ⋁ Y) ¬X ⋀ ¬Y");
+System.Console.WriteLine($"0 0 {Convert.ToInt32(!(falce || false)),5} {Convert.ToInt32(!(falce && false)),8}");
