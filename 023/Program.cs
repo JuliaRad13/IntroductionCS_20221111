8d80df038ec14ac32287ed478a481b03adf478d7
радаева юлия <lulka-malish@mail.ru>
2022-11-18 15:41:45 +0300

    023

---
+++ 023/Program.cs
@@ -0,0 +1,8 @@
+﻿// 23. Найти расстояние между точками в пространстве 2D
+double x1,x2,y1,y2,distance;
+int x1=Convert.ToInt32(Console.ReadLine());
+int x2=Convert.ToInt32(Console.ReadLine());
+int y1=Convert.ToInt32(Console.ReadLine());
+int y2=Convert.ToInt32(Console.ReadLine());
+
+distance=Mat
