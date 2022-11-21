5adf99ff835bac0d0e2510916e7f7cd9edc429bc
радаева юлия <lulka-malish@mail.ru>
2022-11-21 12:50:51 +0300

    020.2

--- 020/Program.cs
+++ 020/Program.cs
@@ -1,7 +1,8 @@
 ﻿// 20. Определить номер четверти плоскости, в которой находится точка с координатами Х и У, 
 //причем X ≠ 0 и Y ≠ 0
 
-double x=Convert.ToDouble(Console.ReadLine());
+
+/*double x=Convert.ToDouble(Console.ReadLine());
 double y=Convert.ToDouble(Console.ReadLine());
 int part=0;
 
@@ -11,3 +12,19 @@ if (x>0 && y<0) part=3;
 if (x>0 && y<0) part=4;
 
 System.Console.WriteLine("Part:"+part);
+*/
+
+//тип имя (аргументы)
+int Part(double x, double y)
+{
+// тело подпрограммы
+    int part=0;
+    if (x>0 && y>0) part=1;
+    if (x<0 && y>0) part=2;
+    if (x>0 && y<0) part=3;
+    if (x>0 && y<0) part=4;
+    return part;
+}
+double x=Convert.ToDouble(Console.ReadLine());
+double y=Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Part:"+Part(x,y));
