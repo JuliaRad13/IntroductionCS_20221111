addfc3c5661bbfd0b9f128694d657573c0fbfd90
радаева юлия <lulka-malish@mail.ru>
2022-11-21 16:18:13 +0300

    031

---
+++ 031/Program.cs
@@ -0,0 +1,26 @@
+﻿// 31. Сгенерировать 10 случайных чисел. 
+//Показать кубы тех чисел, которые заканчивающится на четную цифру
+
+/*int r=10;
+double x=r;// не явное приведение типов
+double x=23;
+//int o=Convert.ToInt32(x);
+int o=(int)x;// явное привидение типов
+*/
+
+bool Test(int a);
+{
+    //if (a%2==0) return true; else return false;
+    return a%2==0;
+}
+
+Random random=new Random(10);
+for(int i=0;i<10;i++)
+{
+    int a=random.Next(1,100);
+    int b=(int)Math.Pow(a,3);
+    if (Test(b))
+    System.Console.WriteLine($"{a}, {b}");
+}
+   
+
