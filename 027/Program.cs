3c70e8ed3cec7f8c032d24953b4516d0ca02824f
радаева юлия <lulka-malish@mail.ru>
2022-11-22 19:31:55 +0300

    027

---
+++ 027/Program.cs
@@ -0,0 +1,14 @@
+﻿// 27. Возведите число А в натуральную степень B используя цикл
+
+System.Console.WriteLine("Введите число A:");
+int A=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число B:");
+int B=Convert.ToInt32(Console.ReadLine());
+int count=1;
+int result=A;
+while (count<B)
+{
+    result=result*A;
+    count++;
+}
+System.Console.WriteLine($"{A}*{B}={result}");
