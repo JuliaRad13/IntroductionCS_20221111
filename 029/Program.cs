c730a48d8cd8a77e27019028aa5ebac243e04d6d
радаева юлия <lulka-malish@mail.ru>
2022-11-22 17:15:46 +0300

    029

---
+++ 029/Program.cs
@@ -0,0 +1,18 @@
+﻿// 26. Подсчитать сумму цифр в числе. Сделать подпрограмму.
+
+System.Console.WriteLine("Введите число:");
+int SumNumbers(int Top)
+{
+    int sum=0;
+    int digit=0;
+    while (Top>0)
+        {
+            digit=Top%10;
+            sum=sum+digit;
+            Top=Top/10;
+        }
+        return sum;
+}
+int X=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Сумма цифр в числе: ");
+System.Console.WriteLine(SumNumbers(X));
