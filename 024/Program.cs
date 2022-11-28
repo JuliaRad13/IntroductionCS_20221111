06477e7df6787b6d092eec213d52395bc6750950
радаева юлия <lulka-malish@mail.ru>
2022-11-28 18:14:20 +0300

    024

--- 024/Program.cs
+++ 024/Program.cs
@@ -1,14 +1,15 @@
 ﻿// 24. Вывести на экран таблицу квадратов чисел от 1 до N
-
+/*
 System.Console.WriteLine("Введите число:");
 int N=Convert.ToInt32(Console.ReadLine());
 for (int a=1;a<=N;a++)
 {
     System.Console.Write($"{a*a} ");
 }
-/*
-System.Console.WriteLine("Введите число:");
-int N=Convert.ToInt32(Console.ReadLine());
-for (int a=1;a<=N;a++)
-System.Console.Write($"{a}^2= ");
 */
+
+//System.Console.WriteLine("Введите число:");
+//int N=Convert.ToInt32(Console.ReadLine());
+int N=10;
+for (int a=1;a<=N;a++)
+System.Console.WriteLine($"{a,4}{a*a,10}");
