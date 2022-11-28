84ecf45830810cc99b3023a92ba835cb50cec4d5
радаева юлия <lulka-malish@mail.ru>
2022-11-28 10:07:28 +0300

    033

---
+++ 033/Program.cs
@@ -0,0 +1,19 @@
+﻿// 33. Задать массив из 8 элементов, заполненных нулями и единицами 
+// вывести их на экран
+
+int[] RandomIntArray(int size, int min, int max) 
+{
+    int[] a=new int[size];
+    Random random=new Random();
+    for (int i = 0; i < size; i++)
+        a[i]=random.Next(min,max+1);
+    return a;
+}
+void Print(int[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+    System.Console.Write($"{a[i],5}");   
+}
+int[] a=RandomIntArray(8,0,1);
+Print(a);
+System.Console.WriteLine();
