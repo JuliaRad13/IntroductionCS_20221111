e92217d7be4fcf0013e42b4c045a423939c79f16
радаева юлия <lulka-malish@mail.ru>
2022-11-29 15:35:11 +0300

    013-014

--- 038/Program.cs
+++ 038/Program.cs
@@ -1,6 +1,7 @@
 ﻿// 38. В одномерном массиве из 123 чисел 
 // найти количество элементов из отрезка [10,99]
 
+/*
 int[] RandomIntArray(int size) 
 {
     int[] a=new int[size];
@@ -25,3 +26,29 @@ int[] a=RandomIntArray(123);
 Print(a);
 System.Console.WriteLine();
 System.Console.WriteLine($"Колличество элементов из отрезка [10.99] равно {Count(a)}");
+*/
+
+// Создали массив
+int N=5;
+int[] a=new int[N];
+// Заполнили массив
+Random random=new Random();
+for (int i = 0; i < a.Length; i++)
+     a[i]=random.Next(0,200);
+for (int i = 0; i < a.Length; i++)
+    System.Console.Write($"{a[i], 5}");
+// Обработка массива
+// 1 способ
+int count=0;
+for (int i = 0; i < a.Length; i++)
+     if(a[i]>=10 && a[i]<=99)
+         count++;
+
+// foreach // коллекция элементов в массиве
+// 2 способ
+count=0;
+foreach (int el in a)
+     if(el>=10 && el<=99)
+         count++;
+System.Console.WriteLine();
+System.Console.WriteLine(count);
---
+++ Basicalgorithms/013 Пузырьковая сортировка/Program.cs
@@ -0,0 +1,39 @@
+﻿// Пузырьковая сортировка
+
+int[] RandomIntArray(int N, int min, int max) 
+{
+    int[] a=new int[N];
+    Random random=new Random();
+    for (int i = 0; i < a.Length; i++)
+        a[i]=random.Next(0, 200);
+    return a;
+}
+void Print(int[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+    System.Console.Write($"{a[i],5}"); 
+}
+void Swap(ref int a, ref int b)
+{
+    int t=a;
+    a=b;
+    b=t;
+}
+
+void BubbleSort(int[] a)// если добавить out int count(сколько раз обработается массив)
+{
+    // count=0;
+    for (int i = 0; i <a.Length; i++)
+        for (int j = 0; j <a.Length-1; j++)// a.Length-1-i добавить i для уменьшения сравнений
+       // {
+            if (a[j]>a[j+1])// сравнить элементы, сортируем
+                Swap(ref a[j],ref a[j+1]);// меняем местами
+           // count++;
+        //}   
+}
+
+int[] a=RandomIntArray(10,0,10);
+Print(a);
+BubbleSort(a);
+System.Console.WriteLine();
+Print(a);
