e0fbf132c6868a0e4f7004ab6bba5e7d4226dfd3
радаева юлия <lulka-malish@mail.ru>
2022-11-30 21:48:16 +0300

    054

---
+++ 051/Program.cs
@@ -0,0 +1,42 @@
+﻿// 51. С клавиатуры вводится число N. Затем вводятся N чисел. 
+// Определить сколько чисел больше 0 введено с клавиатуры
+
+/*
+System.Console.Write("Введите число"); 
+int Count(int a)
+{
+    int count=0;
+    for (int i = 0; i < a; i++)
+        if (a > 0) count++; 
+    return count; 
+}
+int X=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Кол-во элементов > 0: {Count(X)}");
+*/
+
+/*
+Console.Write("Введите число N: ");  
+int q = Convert.ToInt32( Console.ReadLine() );
+int Number (int n=0)
+{
+  int z = 0;
+  for (int i=0 ; i<n; i = i + 1)
+    {    
+        Console.WriteLine("Введите число: "); 
+        int a = Convert.ToInt32( Console.ReadLine());
+        if (a>0) z = z+1; 
+    }   return z;    
+} 
+
+Console.WriteLine($"Больше ноля = {0}", (Number(q)));
+*/
+Console.Write("Введите число: ");
+int[] a = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+ 
+for (int i = 0; i < a.Length; i++)
+    if (a[i] > 0)
+    {
+        count++;
+    }
+Console.WriteLine($"Кол-во элементов > 0: {count}");
---
+++ 054/Program.cs
@@ -0,0 +1,28 @@
+﻿// 54. С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. 
+// Принять первые числа равными 0 и 1
+
+/*
+int n=10;
+int[] a=new int[n];
+a[0]=0;
+a[1]=1;
+for(int i=2; i<n; i++)
+  a[i]=a[i-1]+a[i-2];
+for(int i=0; i<n; i++)
+     System.Console.Write($"{a[i]} ");
+*/
+
+// f(0) =0
+// f(1) =1
+// f(n) = f(n-1) + f(n-2)
+
+int Fibonacci(int n)
+{
+    if(n==0) return 0;
+    else if (n==1) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 0; i < 10; i++)
+{
+    System.Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
