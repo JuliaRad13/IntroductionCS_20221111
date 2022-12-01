966e10d7dd109f7ae13ec47bc9ec27f8dbe17a51
радаева юлия <lulka-malish@mail.ru>
2022-12-01 12:13:59 +0300

    051

--- 051/Program.cs
+++ 051/Program.cs
@@ -1,16 +1,6 @@
 ﻿// 51. С клавиатуры вводится число N. Затем вводятся N чисел. 
 // Определить сколько чисел больше 0 введено с клавиатуры
 
-/*
-System.Console.Write("Введите элементы(через пробел): ");
-int[] a = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-int count = 0;
- 
-for (int i = 0; i < a.Length; i++)
-    if (a[i] > 0) count++;
-System.Console.WriteLine($"Кол-во элементов > 0: {count}");
-*/
-
 int N=3;
 int[] t=new int[N];
 int count=0;
