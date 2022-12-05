1a27790d3bf1b4d14815eb8f5d46ba8f4182e231
радаева юлия <lulka-malish@mail.ru>
2022-12-05 17:50:12 +0300

    071

---
+++ 071/Program.cs
@@ -0,0 +1,23 @@
+﻿// 71. Показать натуральные числа от N до 1, N задано
+/*
+void NaturalNumber(int N, int i)
+{
+    if(N<=i)
+    {
+        System.Console.WriteLine(N);
+        NaturalNumber(N+1, i);
+    }
+}
+NaturalNumber(-10,1);
+*/
+
+// Показать натуральные числа от N до 1, N задано
+void NaturalNumber(int N, int i)
+{
+    if(N>=i)
+    {
+        System.Console.WriteLine(N);
+        NaturalNumber(N-1, i);
+    }
+}
+NaturalNumber(10,1);
