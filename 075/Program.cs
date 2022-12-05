70990c36c119e8f73cc533008f63b0380a960e5d
радаева юлия <lulka-malish@mail.ru>
2022-12-05 21:28:54 +0300

    075

---
+++ 075/Program.cs
@@ -0,0 +1,11 @@
+﻿// 75.Написать программу вычисления функции Аккермана - 5*
+
+
+int Akkerman(int M, int N)
+{
+    if (M == 0) return N + 1;
+    else if (N == 0 && M > 0)
+        return Akkerman(M - 1, 1);
+    else return (Akkerman(M - 1, Akkerman(M, N - 1)));
+}
+System.Console.WriteLine(Akkerman(2,5));
--- 077/Program.cs
+++ 077/Program.cs
@@ -1,11 +1,11 @@
 ﻿// 77. Написать программу показывающие первые N чисел, 
 // для которых каждое следующее равно сумме двух предыдущих. 
 // Первые два элемента последовательности 0 и 1 
-int Fibonacci(int n)
+int Fibonacci(int N)
 {
-    if(n==0) return 0;
-    else if (n==1) return 1;
-    else return Fibonacci(n-1) + Fibonacci(n-2);
+    if(N==0) return 0;
+    else if (N==1) return 1;
+    else return Fibonacci(N-1) + Fibonacci(N-2);
 }
 for (int i = 0; i < 10; i++)
 {
