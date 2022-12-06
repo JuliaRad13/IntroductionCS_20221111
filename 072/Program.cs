b558f282cb83d9eb902d903b401917d86dee6072
радаева юлия <lulka-malish@mail.ru>
2022-12-06 15:23:03 +0300

    072 не работает

--- 072/Program.cs
+++ 072/Program.cs
@@ -1,8 +1,30 @@
 ﻿// 72. Показать натуральные числа от M до N, N и M заданы
 
-double NaturalNumber(int M, int N)
+
+int NaturalNumber(int a, int b)
 {
-    if (N <= M) return NaturalNumber(N+1, M);
-else return NaturalNumber(M+1, N);
+    if (a == 0 && b == 0) return 0;
+    else if (a == b) return a;
+    else if (a < b || a > b)
+        return NaturalNumber(a + 1, b);
+    else return NaturalNumber(b - 1, a);
 }
-NaturalNumber(10, 1);
+System.Console.WriteLine(NaturalNumber(6, 1));
+
+
+int NaturalNumber(int a, int b)
+{
+    if (a == 0 && b == 0) return 0;
+    else if (a == b) return a;
+    else if (a < b)
+    {    
+        System.Console.WriteLine(a);
+        return NaturalNumber(b, a+1);
+    }
+    else if (a > b)
+    {
+        System.Console.WriteLine(b);
+        return NaturalNumber(a, b+1);
+    }
+}
+       System.Console.WriteLine(NaturalNumber(1, 6));
