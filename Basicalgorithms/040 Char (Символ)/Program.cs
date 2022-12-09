7f0efcba4d856565b36ef0718c5cd9005a1f3ffa
радаева юлия <lulka-malish@mail.ru>
2022-12-09 17:15:07 +0300

    040-041

---
+++ Basicalgorithms/040 Char (Символ)/Program.cs
@@ -0,0 +1,14 @@
+﻿char c='a'; //тип char может хранить только 1 символ
+c='\x64'; // указать код символа
+c='\u0064'; // юникод 00(1б) 64(2б)
+char b='a';// char(структура)
+System.Console.WriteLine((int)b);// преобразовать, увидеть код символа, привидение типа
+System.Console.WriteLine(c.ToString()+b.ToString()); //ToString(конвектирует в строку)
+if (b>='0' && b<='9') System.Console.WriteLine("It`s digit"); // проверить на соответствие
+if (b>='A' && b<='Z') System.Console.WriteLine("It`s big Latin");
+if (b>='a' && b<='z' || b>='A' && b<='Z') System.Console.WriteLine("It`s  Latin");
+
+if (char.IsDigit(b)) System.Console.WriteLine("It`s digit");// является ли ввведенный симвод цифрой
+
+
+
---
+++ Basicalgorithms/041 Strings (строки)/Program.cs
@@ -0,0 +1,36 @@
+﻿string s="asdf"; //string(класс)ссылочный тип
+// immutable string (не изменяемые строки)
+System.Console.WriteLine(s[0]);
+for (int i = 0; i < s.Length; i++)
+System.Console.WriteLine(s[i]);
+
+// s[0]='S'; или 3 строки ниже
+char[] cc=s.ToCharArray(); // строчку перебросить в массив символов
+cc[0]='S';// в массиве сделать замену
+s=new String(cc);
+
+System.Text.StringBuilder sb=new System.Text.StringBuilder("asdf"); // обработать
+sb[0]='s';
+s=sb.ToString();
+
+//for (int i = 0; i < 100000; i++)
+//    s=s+"1"; // создается новая строка! так не делай!
+for (int i = 0; i < 100000; i++)
+    sb.Append("1"); // в эгой строке добавляет символ
+
+// пример 1
+string s1="Hello";
+string s2="Hello";
+System.Console.WriteLine((s1==s2)); // сравнить содержимое. s1==s2 или s1.CompareTo(s2)
+
+// пример 2
+Random random1=new Random();
+Random random2=new Random();
+System.Console.WriteLine(random1==random2);// сравнить адреса
+
+// методы, с новой строки
+//s1.Length// длина строки, метод
+//s2=s1.Substring(2);// вырезать часть строки
+//System.Console.WriteLine(s1.IndexOf("as"));// найти совпадение
+//s2=s1.Replace('ab', 'ba'); // менять совпадения
+
