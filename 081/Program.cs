f8d1f47dd894a90ec4bb26b22efc2b1a068e557f
радаева юлия <lulka-malish@mail.ru>
2022-12-09 16:35:30 +0300

    081

---
+++ 081/Program.cs
@@ -0,0 +1,11 @@
+﻿// 81. 81. Вывести на экран символы от A до Z как показано ниже: 
+//A 
+//BB 
+//CCC 
+
+for (char c = 'A'; c <= 'Z'; c++)// перебор символов
+{
+    for (int i = 1; i <= c - 'A' + 1; i++)
+        System.Console.Write(c);
+    System.Console.WriteLine();
+}
